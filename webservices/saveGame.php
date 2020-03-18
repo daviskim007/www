43e52381c2f4827fde55443112d95cfee88a8884
daviskim007 <daviskim007@gmail.com>
2020-03-18 22:45:33 +0900

    20200318

---
+++ webservices/saveGame.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+include "connection.php";
+
+$millisec = $_POST{'millisec'};
+$session = $_POST{'session'};
+$time = $_POST{'time'};
+
+
+$sql = "UPDATE SESSIONS SET time_millisecond = '$millisec', time_display = '$time' WHERE session = '$session'";
+    if(!mysqli_query($GLOBALS['con'],$sql)){
+
+        die("Error in query: ".mysqli_error($con));
+        }
+    else{
+
+       /*
+                $result[] = array(
+                'data' => $session
+
+            );
+            echo json_encode($result);
+
+
+        */
+    }
