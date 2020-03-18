1c0b3a14fd9e4e8b72eeabd02cbc878992f04eeb
daviskim007 <daviskim007@gmail.com>
2020-03-18 12:45:39 +0900

    20200318

--- webservices/startGame.php
+++ webservices/startGame.php
@@ -3,17 +3,26 @@
 include "connection.php";
 
 $token = $_POST{'token'};
+$fb_id = "";
 
-try {
-// Returns a Facebook , FacebookResponse object
-	$response = $fb->get('/me?fields=id,name', $token);
-} 	catch(Facebook\Exceptions\FacebookResponseException $e) {
-	echo 'Graph returned an error: ' . $e->getMessage();
-	exit;
-} 	catch(Facebook\Exceptions\FacebookSDKException $e) {
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;		
-} 
+if($token){
+    try {
+    // Returns a Facebook , FacebookResponse object
+        $response = $fb->get('/me?fields=id,name', $token);
+        $get_data = $response->getDecodedBody();
+        $fb_id = $get_data['id'];
+        echo $fb_id
+
+    } 	catch(Facebook\Exceptions\FacebookResponseException $e) {
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } 	catch(Facebook\Exceptions\FacebookSDKException $e) {
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+}else{
+
+}
 
 $user = $response->getGraphUser();	
 
