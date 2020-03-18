e6366ce444526317a22e9b54d3d9cc93e5ce047f
daviskim007 <daviskim007@gmail.com>
2020-03-18 13:28:33 +0900

    20200318

--- webservices/startGame.php
+++ webservices/startGame.php
@@ -5,6 +5,26 @@ include "connection.php";
 $token = $_POST{'token'};
 $fb_id = "";
 
+// generate-random-alphanumeric-strings-in-php-
+// from https://code.tutsplus.com/tutorials/generate-random-alphanumeric-strings-in-php--cms-32132
+
+$permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function generate_string($input, $strength = 16) {
+    $input_length = strlen($input);
+    $random_string = '';
+    for($i = 0; $i < $strength; $i++) {
+        $random_character = $input[mt_rand(0, $input_length - 1)];
+        $random_string .= $random_character;
+    }
+
+    return $random_string;
+}
+
+$session = generate_string($permitted_chars, 20);
+
+
+
 if($token){
     try {
     // Returns a Facebook , FacebookResponse object
@@ -12,6 +32,20 @@ if($token){
         $get_data = $response->getDecodedBody();
         $fb_id = $get_data['id'];
         echo $fb_id;
+
+        $sql = "INSERT INTO SESSIONS (session, facebook_id,time_millisecond), VALUES ('$session','$fb_id',0)";
+        if(!mysqli_query($GLOBALS['con'],$sql)){
+
+            die("Error in query: ".mysqli_error($con));
+            }else{
+                $result[] = array(
+                    'data' => $session
+
+                )
+                echo json_encode($result);
+
+
+            }
 
     } 	catch(Facebook\Exceptions\FacebookResponseException $e) {
         echo 'Graph returned an error: ' . $e->getMessage();
