80a028c975beedfd0de15af9595403bf7efc228e
daviskim007 <daviskim007@gmail.com>
2020-03-21 16:20:16 +0900

    2020-03-21 16:00pm Hijacking global variable and how to protect them

--- scripts/main.js
+++ scripts/main.js
@@ -1,281 +1,321 @@
-var cardContent = ['1','2','3','4','5','6','7','8','9','10','1','2','3','4','5','6','7','8','9','10'];
-var cardClicks = 0;
+window.addEventListener("load", init);
 
-var firstSelection;
-var secondSelection;
-var previousCard;
-var intervalTimer;
-var gameStartTime;
-var matchCount = 0;
+// All the codes must be here to prevent to change the global variables without the Facebook function because they are asynchronous calls.
+function init() {
+	var cardContent = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+	var cardClicks = 0;
 
-var FB_TOKEN;
-var URL = "https://broccolisys.com/webservices/";
-var gameSession ="";
+	var firstSelection;
+	var secondSelection;
+	var previousCard;
+	var intervalTimer;
+	var gameStartTime;
+	var matchCount = 0;
 
-function openGame(){
-	console.log("GAME");
-	$('.game-container').show();
-	$('.highscore-container').hide();
-	$('#but-game').addClass("tab-active");
-	$('#but-highscore').removeClass("tab-active");
-}
+	var FB_TOKEN;
+	var URL = "https://broccolisys.com/webservices/";
+	var gameSession = "";
 
-function openHighscore(){
-	console.log("HIGHSCORE");
-	getHighScores();
-	$('.game-container').hide();
-	$('.highscore-container').show();
-	$('#but-game').removeClass("tab-active");
-	$('#but-highscore').addClass("tab-active");
-}
+	$('.play-button').onclick(function () {closeIntroPopin()});
+	$('.but-game').onclick(function () {openGame()});
+	$('.but-highscore').onclick(function () {openHighscore()});
 
-function closeIntroPopin(){
-	$(".intro-popin-container").fadeOut();
-	initialise();
-}
+	but-game
+	function openGame() {
+		console.log("GAME");
+		$('.game-container').show();
+		$('.highscore-container').hide();
+		$('#but-game').addClass("tab-active");
+		$('#but-highscore').removeClass("tab-active");
+	}
 
-function initialise(){
+	function openHighscore() {
+		console.log("HIGHSCORE");
+		getHighScores();
+		$('.game-container').hide();
+		$('.highscore-container').show();
+		$('#but-game').removeClass("tab-active");
+		$('#but-highscore').addClass("tab-active");
+	}
 
-	cardContent = shuffle(cardContent);
-	// targeting container
-	for(var i=0;i<20;i++){
-		var card="<div class='flip-card'><div class='flip-card-inner' id='card"+i+"' onclick='selectCard("+i+","+cardContent[i]+")'><" +
-			"div class='flip-card-front'></div><div class='flip-card-back' style='background-image: " +
-			"url(img/card"+cardContent[i]+".png)'></div></div></div>";
+	function closeIntroPopin() {
+		$(".intro-popin-container").fadeOut();
+		initialise();
+	}
 
-		$(".card-container").append(card);
+	function initialise() {
+
+		cardContent = shuffle(cardContent);
+		// targeting container
+		for (var i = 0; i < 20; i++) {
+			var card = "<div class='flip-card'><div class='flip-card-inner' id='card" + i + "'><" +
+				"div class='flip-card-front'></div><div class='flip-card-back' style='background-image: " +
+				"url(img/card" + cardContent[i] + ".png)'></div></div></div>";
+
+			$(".card-container").append(card);
+
+		}
+
+		$('#card0').onclick(function () {selectCard(0, cardContent[0])});
+		$('#card1').onclick(function () {selectCard(1, cardContent[1])});
+		$('#card2').onclick(function () {selectCard(2, cardContent[2])});
+		$('#card3').onclick(function () {selectCard(3, cardContent[3])});
+		$('#card4').onclick(function () {selectCard(4, cardContent[4])});
+		$('#card5').onclick(function () {selectCard(5, cardContent[5])});
+		$('#card6').onclick(function () {selectCard(6, cardContent[6])});
+		$('#card7').onclick(function () {selectCard(7, cardContent[7])});
+		$('#card8').onclick(function () {selectCard(8, cardContent[8])});
+		$('#card9').onclick(function () {selectCard(9, cardContent[9])});
+		$('#card10').onclick(function () {selectCard(10, cardContent[10])});
+		$('#card11').onclick(function () {selectCard(11, cardContent[11])});
+		$('#card12').onclick(function () {selectCard(12, cardContent[12])});
+		$('#card13').onclick(function () {selectCard(13, cardContent[13])});
+		$('#card14').onclick(function () {selectCard(14, cardContent[14])});
+		$('#card15').onclick(function () {selectCard(15, cardContent[15])});
+		$('#card16').onclick(function () {selectCard(16, cardContent[16])});
+		$('#card17').onclick(function () {selectCard(17, cardContent[17])});
+		$('#card18').onclick(function () {selectCard(18, cardContent[18])});
+		$('#card19').onclick(function () {selectCard(19, cardContent[19])});
+
+		/*Kick it after 1000 ms. When we are adding all the cards dynamically, it will take a small lapse of time
+        * before all the image are loaded. So it's a good thing that we put a small delay here so that the user have
+        * some time before starting the game*/
+		setTimeout(function () {
+			startTimer(), 1000
+		})
+	}
+
+	function startTimer() {
+		startGameServer();
+		gameStartTime = new Date().getTime();
+		intervalTimer = setInterval(incrementTime, 100);
+	}
+
+	/*This function are going to upda	// targeting containerte the container here the div with the timer container and update it with the time
+    * in terms of seconds minutes and milliseconds */
+	function incrementTime() {
+		/*We are going to use the code once more once the game is over. */
+		var currentTime = gameCounterTime();
+		$(".timer").text(currentTime);
 
 	}
 
-	/*Kick it after 1000 ms. When we are adding all the cards dynamically, it will take a small lapse of time
-	* before all the image are loaded. So it's a good thing that we put a small delay here so that the user have
-	* some time before starting the game*/
-	setTimeout(function(){startTimer(),1000})
-}
+	/*This function is going to return the time that has elapsed since the start of the game*/
+	function gameCounterTime() {
+		var newTime = new Date().getTime();
 
-function startTimer() {
-	startGameServer();
-	gameStartTime = new Date().getTime();
-	intervalTimer = setInterval(incrementTime, 100);
-}
+		var difference = newTime - gameStartTime;
 
-/*This function are going to upda	// targeting containerte the container here the div with the timer container and update it with the time
-* in terms of seconds minutes and milliseconds */
-function incrementTime() {
-	/*We are going to use the code once more once the game is over. */
-	var currentTime = gameCounterTime();
-	$(".timer").text(currentTime);
+		// 73500 milliseconds
+		// 73500 % 60.000 = 13500
+		// 13500 / 1000 = 13.5
+		// Floor = 13 seconds
 
-}
-/*This function is going to return the time that has elapsed since the start of the game*/
-function gameCounterTime() {
-	var newTime = new Date().getTime();
+		var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+		var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+		var milli = difference % 1000;
 
-	var difference = newTime - gameStartTime;
+		seconds = ('0' + seconds).slice(-2);
+		minutes = ('0' + minutes).slice(-2);
 
-	// 73500 milliseconds
-	// 73500 % 60.000 = 13500
-	// 13500 / 1000 = 13.5
-	// Floor = 13 seconds
+		var displayText = minutes + ":" + seconds + ":" + milli;
 
-	var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-	var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000*60));
-	var milli = difference % 1000;
+		return displayText;
 
-	seconds = ('0' + seconds).slice(-2);
-	minutes = ('0' + minutes).slice(-2);
+	}
 
-	var displayText = minutes + ":" + seconds + ":" + milli;
+	function selectCard(id, current) {
+		console.log(id, current);
+		var cardID = "#card" + id;
 
-	return displayText;
+		if (!$(cardID).hasClass('rotate180' && cardClicks != 2) & cardClicks != 2) {
+			$(cardID).addClass('rotate180');
 
-}
+			cardClicks++;
 
-function selectCard(id,current){
-	console.log(id, current);
-	var cardID = "#card"+id;
+			if (cardClicks == 1) {
+				firstSelection = current;
+				previousCard = cardID;
+			}
 
-	if(!$(cardID).hasClass('rotate180' && cardClicks!=2) & cardClicks !=2){
-		$(cardID).addClass('rotate180');
+			if (cardClicks == 2) {
+				secondSelection = current;
 
-		cardClicks++;
+				if (firstSelection == secondSelection) {
+					console.log('Match!!');
 
-		if (cardClicks==1){
-			firstSelection = current;
-			previousCard = cardID;
-		}
+					matchCount++;
+					if (matchCount == 2) {
+						clearInterval(intervalTimer);
+						var currentTime = gameCounterTime();
+						$(".timer").text(currentTime);
 
-		if (cardClicks==2){
-			secondSelection = current;
+						setTimeout(function () {
+							$(".game-finish").show()
+						}, 1000);
 
-			if(firstSelection == secondSelection){
-				console.log('Match!!');
+						var newTime = new Date().getTime();
+						var timeDifferenceMilli = newTime - gameStartTime;
 
-				matchCount++;
-				if(matchCount ==2)	{
-					clearInterval(intervalTimer);
-					var currentTime = gameCounterTime();
-					$(".timer").text(currentTime);
+						if (!FB_TOKEN) {
+							$('.save-message').show();
+						}
 
-					setTimeout(function(){$(".game-finish").show()},1000);
-
-					var newTime = new Date().getTime();
-					var timeDifferenceMilli = newTime - gameStartTime;
-
-					if(!FB_TOKEN){
-						$('.save-message').show();
+						saveGame(timeDifferenceMilli, currentTime);
 					}
 
-					saveGame(timeDifferenceMilli, currentTime);
+					setTimeout(function () {
+						cardClicks = 0;
+					}, 500)
+				} else {
+					setTimeout(function () {
+						$(cardID).removeClass('rotate180');
+						$(previousCard).removeClass('rotate180');
+						cardClicks = 0;
+					}, 500)
+
 				}
 
-				setTimeout(function(){
-					cardClicks=0;
-				},500)
-			}else{
-				setTimeout(function(){
-					$(cardID).removeClass('rotate180');
-					$(previousCard).removeClass('rotate180');
-					cardClicks=0;
-				},500)
-
 			}
-
 		}
+
 	}
 
-}
+	/* Get high scores. This function is to clear the content of this high scores class so that each time you click on the
+    high score button it is going to refresh the table*/
+	function getHighScores() {
+		// Remove all the contents of the div
+		$('.highscores').empty();
+		// Call the getTopScores and we will wait for a response before proceeding to the next part.
+		getTopScores(function (response) {
+			// If we are getting any response then we are going to proceed with the next piece of code and in order to populate each row for
+			// each player we need to put a for loop and then add each row in the high score container.
+			if (response) {
+				for (var index = 0; index < response.length; index++) {
+					var user = response[index];
+					// Item with the information for each player and profile row
+					var item = "<div class ='profile-row'><div class='profile-image' style='background-image:url(" + user.userPic + ")'></div>" +
+						"<div class='profile-name'>" + user.fullname + "</div><div class='profile-time'>" + user.time + "</div></div>";
 
-/* Get high scores. This function is to clear the content of this high scores class so that each time you click on the
-high score button it is going to refresh the table*/
-function getHighScores() {
-	// Remove all the contents of the div
-	$('.highscores').empty();
-	// Call the getTopScores and we will wait for a response before proceeding to the next part.
-	getTopScores(function (response) {
- 		// If we are getting any response then we are going to proceed with the next piece of code and in order to populate each row for
-		// each player we need to put a for loop and then add each row in the high score container.
-		if(response){
-			for (var index=0; index<response.length; index++){
-				var user = response[index];
-				// Item with the information for each player and profile row
-				var item = "<div class ='profile-row'><div class='profile-image' style='background-image:url("+user.userPic+")'></div>" +
-					"<div class='profile-name'>"+user.fullname+"</div><div class='profile-time'>"+user.time+"</div></div>";
-
-				$('.highscores').append(item);
-				// Display each response in a different row
-				console.log(response[index])
+					$('.highscores').append(item);
+					// Display each response in a different row
+					console.log(response[index])
+				}
 			}
-		}
 
-	})
-}
+		})
+	}
 
 // Call the web service just like we have done here and do the same for calling the high score web service.
-function getTopScores(callback) {
+	function getTopScores(callback) {
 
-	console.log("HIGHSCORES");
+		console.log("HIGHSCORES");
 
-	$.ajax({
-		type:"POST",
-		data:{},
-		url : "https://broccolisys.com/webservices/topScores.php",
-		// url: URL + "startGame.php",
-		success : function(data){
-			console.log(data);
-			// Send back the data that we have received from here.
-			callback(JSON.parse(data))
-		},
-		error : function(data){
-			console.log(data)
-		}
-	})
-}
+		$.ajax({
+			type: "POST",
+			data: {},
+			url: "https://broccolisys.com/webservices/topScores.php",
+			// url: URL + "startGame.php",
+			success: function (data) {
+				console.log(data);
+				// Send back the data that we have received from here.
+				callback(JSON.parse(data))
+			},
+			error: function (data) {
+				console.log(data)
+			}
+		})
+	}
 
-function saveGame(timeDifferenceMilli, currentTime) {
-	// Put a KEY which needs to be 16 bits
-	var KEY = 'memorygame007007';
-	// Put the string which are only numbers for this case
-	var IV = '1234567890123412';
-	var key = CryptoJS.enc.Utf8.parse(KEY); // Secret Key
-	var iv = CryptoJS.enc.Utf8.parse(IV); // Vector iv
+	function saveGame(timeDifferenceMilli, currentTime) {
+		// Put a KEY which needs to be 16 bits
+		var KEY = 'memorygame007007';
+		// Put the string which are only numbers for this case
+		var IV = '1234567890123412';
+		var key = CryptoJS.enc.Utf8.parse(KEY); // Secret Key
+		var iv = CryptoJS.enc.Utf8.parse(IV); // Vector iv
 
-	var myData = {'session': gameSession, 'millisec': timeDifferenceMilli, 'time':currentTime, 'token': FB_TOKEN };
+		var myData = {'session': gameSession, 'millisec': timeDifferenceMilli, 'time': currentTime, 'token': FB_TOKEN};
 
-	//  Stringify a data
-	var secret = JSON.stringify(myData);
+		//  Stringify a data
+		var secret = JSON.stringify(myData);
 
-	// CryptoJS library convert data witch is being stringify data and pass the server
-	var encrypted = CryptoJS.AES.encrypt(secret, key, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7}).toString();
+		// CryptoJS library convert data witch is being stringify data and pass the server
+		var encrypted = CryptoJS.AES.encrypt(secret, key, {
+			iv: iv,
+			mode: CryptoJS.mode.CBC,
+			padding: CryptoJS.pad.Pkcs7
+		}).toString();
 
-	console.log("SAVING GAME");
+		console.log("SAVING GAME");
 
-	$.ajax({
-		type:"POST",
-		data:{data:encrypted},
-		url : "https://broccolisys.com/webservices/saveGame.php",
-		// url: URL + "startGame.php",
-		success : function(data){
-			console.log(data);
-		},
-		error : function(data){
-			console.log(data)
-		}
-	})
-}
+		$.ajax({
+			type: "POST",
+			data: {data: encrypted},
+			url: "https://broccolisys.com/webservices/saveGame.php",
+			// url: URL + "startGame.php",
+			success: function (data) {
+				console.log(data);
+			},
+			error: function (data) {
+				console.log(data)
+			}
+		})
+	}
 
-function savePlayer() {
+	function savePlayer() {
 
-	console.log("SAVING Player");
+		console.log("SAVING Player");
 
-	$.ajax({
-		type:"POST",
-		data:{'session' : gameSession, 'token' : FB_TOKEN},
-		url : "https://broccolisys.com/webservices/savePlayer.php",
-		success : function(data){
-			console.log(data);
-		},
-		error : function(data, textStatus, error){
-			console.log(data);
-		}
-	})
-}
+		$.ajax({
+			type: "POST",
+			data: {'session': gameSession, 'token': FB_TOKEN},
+			url: "https://broccolisys.com/webservices/savePlayer.php",
+			success: function (data) {
+				console.log(data);
+			},
+			error: function (data, textStatus, error) {
+				console.log(data);
+			}
+		})
+	}
 
-function startGameServer(){
-	console.log("Starting game on Server");
-	$.ajax({
-		type:"POST",
-		data:{'token' : FB_TOKEN},
-		url : "https://broccolisys.com/webservices/startGame.php",
-		// url: URL + "startGame.php",
-		success : function(data){
-			console.log(data);
-			r = JSON.parse(data);
-			gameSession = r[0].data;
-			console.log("gameSession",gameSession);
-		},
-		error : function(data){
-			console.log(data)
-		}
-	})
-}
+	function startGameServer() {
+		console.log("Starting game on Server");
+		$.ajax({
+			type: "POST",
+			data: {'token': FB_TOKEN},
+			url: "https://broccolisys.com/webservices/startGame.php",
+			// url: URL + "startGame.php",
+			success: function (data) {
+				console.log(data);
+				r = JSON.parse(data);
+				gameSession = r[0].data;
+				console.log("gameSession", gameSession);
+			},
+			error: function (data) {
+				console.log(data)
+			}
+		})
+	}
 
-function shuffle(arra1) {
-	var ctr = arra1.length, temp, index;
+	function shuffle(arra1) {
+		var ctr = arra1.length, temp, index;
 
 // While there are elements in the array
-	while (ctr > 0) {
+		while (ctr > 0) {
 // Pick a random index
-		index = Math.floor(Math.random() * ctr);
+			index = Math.floor(Math.random() * ctr);
 // Decrease ctr by 1
-		ctr--;
+			ctr--;
 // And swap the last element with it
-		temp = arra1[ctr];
-		arra1[ctr] = arra1[index];
-		arra1[index] = temp;
+			temp = arra1[ctr];
+			arra1[ctr] = arra1[index];
+			arra1[index] = temp;
 
+		}
+		return arra1;
 	}
-	return arra1;
 }
 
 // FACEBOOK CALLS 
